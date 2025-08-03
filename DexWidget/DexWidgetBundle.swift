a04b80fcfc4a1f8074b6dd0a3f00b1066298503e
aravind vallabhaneni <aravindvalla07@gmail.com>
2025-08-04 00:31:14 +0530

    Added widgetExtension to the project

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Aravind vallabhaneni on 04/08/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
